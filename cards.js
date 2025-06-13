e7710523e67b083afa94d8222dd3fa03a68baad6
crixppp <samgilliot1@gmail.com>
2025-06-14 01:33:30 +1000

    Create cards.js

---
+++ cards.js
@@ -0,0 +1,45 @@
+// cards.js
+export function createDeck() {
+  const suits = ['♠', '♥', '♦', '♣'];
+  const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+  const deck = [];
+
+  for (const suit of suits) {
+    for (const value of values) {
+      deck.push({ suit, value });
+    }
+  }
+
+  return shuffle(deck);
+}
+
+function shuffle(deck) {
+  for (let i = deck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [deck[i], deck[j]] = [deck[j], deck[i]];
+  }
+  return deck;
+}
+
+export function calculateValue(hand) {
+  let total = 0;
+  let aces = 0;
+
+  for (const card of hand) {
+    if (['J', 'Q', 'K'].includes(card.value)) {
+      total += 10;
+    } else if (card.value === 'A') {
+      total += 11;
+      aces++;
+    } else {
+      total += parseInt(card.value);
+    }
+  }
+
+  while (total > 21 && aces > 0) {
+    total -= 10;
+    aces--;
+  }
+
+  return total;
+}
